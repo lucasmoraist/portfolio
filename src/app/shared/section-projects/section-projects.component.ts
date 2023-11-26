249e48ff77936025a27653d3bd4cb6b89970a15a
lucasmoraist <luksmnt1101@gmail.com>
2023-11-26 12:57:23 -0300

    alterando seletor de projects

--- src/app/shared/section-projects/section-projects.component.ts
+++ src/app/shared/section-projects/section-projects.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-section-projects',
+  selector: 'section-projects',
   templateUrl: './section-projects.component.html',
   styleUrls: ['./section-projects.component.css']
 })
