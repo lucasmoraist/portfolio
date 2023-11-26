3c35bdf60f7d24c92d4274b7cef5756d4b724a61
lucasmoraist <luksmnt1101@gmail.com>
2023-11-26 15:00:24 -0300

    add components de contato, linkedin

---
+++ src/app/shared/linkedin-contact/linkedin-contact.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'linkedin-contact',
+  templateUrl: './linkedin-contact.component.html',
+  styleUrls: ['./linkedin-contact.component.css']
+})
+export class LinkedinContactComponent {
+
+}
