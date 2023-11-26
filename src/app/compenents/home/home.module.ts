2c7b668cf38eaeaa67a5bef9ffe9dbb0a6cb8f5f
lucasmoraist <luksmnt1101@gmail.com>
2023-11-26 12:57:36 -0300

    add components a tela de home

--- src/app/compenents/home/home.module.ts
+++ src/app/compenents/home/home.module.ts
@@ -7,15 +7,20 @@ import { BtnPrimaryComponent } from '../../shared/btn-primary/btn-primary.compon
 import { MainSectionComponent } from 'src/app/shared/main-section/main-section.component';
 import { MainImageComponent } from 'src/app/shared/main-image/main-image.component';
 import { SectionAboutComponent } from 'src/app/shared/section-about/section-about.component';
-
+import { SectionContactComponent } from 'src/app/shared/section-contact/section-contact.component';
+import { FooterComponent } from 'src/app/shared/footer/footer.component';
+import { SectionProjectsComponent } from 'src/app/shared/section-projects/section-projects.component';
 @NgModule({
   declarations: [
     HomeComponent,
-    HeaderComponent,
-    BtnPrimaryComponent,
-    MainSectionComponent,
-    MainImageComponent,
-    SectionAboutComponent
+    HeaderComponent, //cabeçalho
+    BtnPrimaryComponent, //botão
+    MainSectionComponent, //seção main
+    MainImageComponent, //imagem em svg para seção main
+    SectionAboutComponent, //seção sobre mim
+    SectionProjectsComponent,
+    SectionContactComponent, //seção com contatos
+    FooterComponent //rodapé
   ],
   imports: [
     BrowserModule
