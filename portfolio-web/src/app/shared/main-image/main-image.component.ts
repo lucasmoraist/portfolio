3d3f50cea483bcd3a5818f83e3ac0d609d80201f
lucasmoraisnt <rm93267@fiap.com.br>
2023-11-25 23:08:01 -0300

    criando componente para imagem da main

---
+++ portfolio-web/src/app/shared/main-image/main-image.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'main-image',
+  templateUrl: './main-image.component.html',
+  styleUrls: ['./main-image.component.css']
+})
+export class MainImageComponent {
+
+}
