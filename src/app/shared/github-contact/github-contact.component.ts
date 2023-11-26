14a2feb4cdc4bc3043f8c68a8f5d1c6f9c980993
lucasmoraist <luksmnt1101@gmail.com>
2023-11-26 14:59:59 -0300

    add components de contato, github

---
+++ src/app/shared/github-contact/github-contact.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'github-contact',
+  templateUrl: './github-contact.component.html',
+  styleUrls: ['./github-contact.component.css']
+})
+export class GithubContactComponent {
+
+}
