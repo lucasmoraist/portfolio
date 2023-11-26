a9b8b69c0ce09756e2a8895d6612cb5cb8f9ec59
lucasmoraist <luksmnt1101@gmail.com>
2023-11-26 12:30:08 -0300

    add seção sobre mim

---
+++ src/app/shared/section-about/section-about.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'section-about',
+  templateUrl: './section-about.component.html',
+  styleUrls: ['./section-about.component.css']
+})
+export class SectionAboutComponent {
+
+}
