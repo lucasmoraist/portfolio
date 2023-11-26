4c0e067b8b31658dbc5049da669a8fee616c5073
lucasmoraist <luksmnt1101@gmail.com>
2023-11-26 20:54:12 -0300

    done

--- src/app/compenents/home/home.module.ts
+++ src/app/compenents/home/home.module.ts
@@ -1,8 +1,4 @@
-<<<<<<< HEAD
 import { NgModule } from '@angular/core';
-=======
-import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core';
->>>>>>> origin/develop
 import { BrowserModule } from '@angular/platform-browser';
 
 import { HomeComponent } from './home.component';
@@ -10,7 +6,13 @@ import { HeaderComponent } from '../../shared/header/header.component';
 import { BtnPrimaryComponent } from '../../shared/btn-primary/btn-primary.component';
 import { MainSectionComponent } from 'src/app/shared/main-section/main-section.component';
 import { MainImageComponent } from 'src/app/shared/main-image/main-image.component';
-<<<<<<< HEAD
+import { SectionProjectsComponent } from 'src/app/shared/section-projects/section-projects.component';
+import { SectionContactComponent } from 'src/app/shared/section-contact/section-contact.component';
+import { FooterComponent } from 'src/app/shared/footer/footer.component';
+import { GithubContactComponent } from 'src/app/shared/github-contact/github-contact.component';
+import { LinkedinContactComponent } from 'src/app/shared/linkedin-contact/linkedin-contact.component';
+import { GmailContactComponent } from 'src/app/shared/gmail-contact/gmail-contact.component';
+import { SectionAboutComponent } from 'src/app/shared/section-about/section-about.component';
 
 @NgModule({
   declarations: [
@@ -18,43 +20,22 @@ import { MainImageComponent } from 'src/app/shared/main-image/main-image.compone
     HeaderComponent,
     BtnPrimaryComponent,
     MainSectionComponent,
-    MainImageComponent
-=======
-import { SectionAboutComponent } from 'src/app/shared/section-about/section-about.component';
-import { SectionContactComponent } from 'src/app/shared/section-contact/section-contact.component';
-import { FooterComponent } from 'src/app/shared/footer/footer.component';
-import { SectionProjectsComponent } from 'src/app/shared/section-projects/section-projects.component';
-import { GmailContactComponent } from 'src/app/shared/gmail-contact/gmail-contact.component';
-import { LinkedinContactComponent } from 'src/app/shared/linkedin-contact/linkedin-contact.component';
-import { GithubContactComponent } from 'src/app/shared/github-contact/github-contact.component';
-@NgModule({
-  declarations: [
-    HomeComponent,
-    HeaderComponent, //cabeçalho
-    BtnPrimaryComponent, //botão
-    MainSectionComponent, //seção main
-    MainImageComponent, //imagem em svg para seção main
-    SectionAboutComponent, //seção sobre mim
+    MainImageComponent,
+    SectionAboutComponent,
     SectionProjectsComponent,
-    SectionContactComponent, //seção com contatos
-    FooterComponent, //rodapé
+    SectionContactComponent,
+    FooterComponent,
 
     //Contatos
-    LinkedinContactComponent,
-    GmailContactComponent,
     GithubContactComponent,
->>>>>>> origin/develop
+    LinkedinContactComponent,
+    GmailContactComponent
   ],
   imports: [
     BrowserModule
   ],
   providers: [],
   bootstrap: [],
-<<<<<<< HEAD
   exports: [HomeComponent]
-=======
-  exports: [HomeComponent],
-  schemas: [ CUSTOM_ELEMENTS_SCHEMA ]
->>>>>>> origin/develop
 })
 export class HomeModule { }
