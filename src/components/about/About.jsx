952cc52eac3dfd7aaaa697251388bd87bfef65cb
lucasmoraist <luksmnt1101@gmail.com>
2024-05-19 23:46:22 -0300

    feat: changing stack

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -5,7 +5,7 @@ const About = () => {
     return <AboutStyled>
 
         <p>
-            Me chamo de Lucas de Morais, sou Dev Full Stack! Apaixonado por programação desde os 18 anos, sou de São Paulo Capital,
+            Me chamo de Lucas de Morais, sou Dev Backend! Apaixonado por programação desde os 18 anos, sou de São Paulo Capital,
             nascido em 2004. Sou Formado em Análise e Desenvolvimento de Software, na FIAP. Além da programação eu também adoro praticar
             esportes e com ele aprendi a trabalhar muito bem equipe e me esforçar para dar ótimos resultados individualmente.
             Nas horas vagas gosto de assistir animes, jogar jogos eletrônicos, ir a academia ou praticar algum esporte.
